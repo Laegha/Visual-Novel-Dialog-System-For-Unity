ec6ed3245eee4e3077a31830500fd4c7c9fe0e0d
Laegha <franciscolarghero@gmail.com>
2024-03-26 22:51:36 -0300

    feat(DialogTree): DialogTree ScriptableObject

---
+++ Dialog System/Assets/Scripts/Dialogs/DialogTrees/DialogTree.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialog", menuName = "ScriptableObjects/Dialogs/DialogTree", order = 1)]
+public class DialogTree : ScriptableObject
+{
+    Dialog[] dialogs;
+
+}

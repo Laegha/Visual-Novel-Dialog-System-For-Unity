c765180d5a0ee820a95accd6cb989557ffc3c6fd
Laegha <franciscolarghero@gmail.com>
2024-04-11 23:27:07 -0300

    refactor(DialogTree): SerializedDictionary to regular Dictionary

--- Dialog System/Assets/Scripts/Dialogs/DialogTrees/DialogTree.cs
+++ Dialog System/Assets/Scripts/Dialogs/DialogTrees/DialogTree.cs
@@ -1,11 +1,11 @@
-using AYellowpaper.SerializedCollections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewDialog", menuName = "ScriptableObjects/Dialogs/DialogTree", order = 1)]
 public class DialogTree : ScriptableObject
 {
-    public SerializedDictionary<string, Dialog> dialogs = new SerializedDictionary<string, Dialog>();
+    public Dictionary<string, Dialog> dialogs = new Dictionary<string, Dialog>();
     
 
-    public SerializedDictionary<string, Dialog> Dialogs { set{ dialogs = value; } get { return dialogs; } }
+    public Dictionary<string, Dialog> Dialogs { set{ dialogs = value; } get { return dialogs; } }
 }

a3e8b3015a83942b92f4e2885a4d659193015566
Laegha <franciscolarghero@gmail.com>
2024-06-03 01:30:50 -0300

    refactor(DialogNode): IsInitial instead of index
    As we are no longer working with a full list of Dialogs, we only care about if its the initialDialog or not

--- Dialog System/Assets/Scripts/Dialogs/DialogTrees/DialogNode.cs
+++ Dialog System/Assets/Scripts/Dialogs/DialogTrees/DialogNode.cs
@@ -4,16 +4,18 @@ using UnityEngine.UIElements;
 
 public class DialogNode : ScriptableObject
 {
+    bool isInitial;
+
     [SerializeField] Dialog dialog;
-    [SerializeField] Dialog prevDialog;
-    int dialogIndex;
+    Dialog prevDialog;
     DialogNodeView dialogNodeView;
     List<DialogConnection> inputConnections = new List<DialogConnection>();
     List<DialogConnection> outputConnections = new List<DialogConnection>();
 
+    public bool IsInitial{ set { isInitial = value; } get{ return isInitial; } }
+
     public Dialog Dialog { set { dialog = value; } get{ return dialog; } }
     public Dialog PrevDialog { set { prevDialog = value; } get{ return prevDialog; } }
-    public int DialogIndex{ set { dialogIndex = value; } get{ return dialogIndex; } }
     public DialogNodeView View{ set { dialogNodeView = value; } get{ return dialogNodeView; } }
     public List<DialogConnection> InputConnections { set { inputConnections = value; } get{ return inputConnections; } }
     public List<DialogConnection> OutputConnections { set { outputConnections = value; } get{ return outputConnections; } }

99e64568f013338baa6a5afb5d75e1c5bb3595d6
Laegha <franciscolarghero@gmail.com>
2024-06-10 02:07:48 -0300

    feat(DialogTree): DialogData and UnusedNodes
    UnusedNodes stores the DialogData of the nodes that had no input when the graph editor was left, so no progress is lost when working on a tree

--- Dialog System/Assets/Scripts/DialogTrees/DialogTree.cs
+++ Dialog System/Assets/Scripts/DialogTrees/DialogTree.cs
@@ -1,8 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewDialogTree", menuName = "ScriptableObjects/Dialogs/DialogTree", order = 1)]
 public class DialogTree : ScriptableObject
 {
-    public Dialog initialDialog;
+    DialogData initialDialog = null;
+    List<DialogData> unusedNodes = new List<DialogData>();
+
+    public DialogData InitialDialog { get { return initialDialog; } set { initialDialog = value; } }
+    public List<DialogData> UnusedNodes { get { return unusedNodes; } set { unusedNodes = value; } }
     //probably add things related to the main character AKA protagonist
 }
